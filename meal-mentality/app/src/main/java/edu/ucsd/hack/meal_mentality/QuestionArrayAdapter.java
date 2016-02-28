fa5a71e1c74dd13836efa54c66daf2d125f03830
Chirag Toprani <chiragT.007@gmail.com>
2016-02-28 02:42:53 -0800

    save radio button values

--- meal-mentality/app/src/main/java/edu/ucsd/hack/meal_mentality/QuestionArrayAdapter.java
+++ meal-mentality/app/src/main/java/edu/ucsd/hack/meal_mentality/QuestionArrayAdapter.java
@@ -3,11 +3,15 @@ package edu.ucsd.hack.meal_mentality;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -23,8 +27,13 @@ public class QuestionArrayAdapter extends ArrayAdapter<Question> {
 
     private class ViewHolder {
         private TextView line1;
+        private EditText val;
+        private int rb_checked;
 
+        private ViewHolder() {
+        }
     }
+
     private static final String TAG = "QuestionArrayAdapter";
     private List<Question> questList = new ArrayList<Question>();
     public QuestionArrayAdapter(Context context, int textViewResourceId) {
@@ -42,9 +51,19 @@ public class QuestionArrayAdapter extends ArrayAdapter<Question> {
     }
 
     @Override
-    public Question getItem(int index) {
-        return this.questList.get(index);
+    public int getViewTypeCount() {
+        //Count=Size of ArrayList.
+        return questList.size();
     }
+
+    @Override
+    public int getItemViewType(int position) {
+
+        return position;
+    }
+
+
+
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -57,9 +76,22 @@ public class QuestionArrayAdapter extends ArrayAdapter<Question> {
             LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             if(card.type == "YN") {
                 row = inflater.inflate(R.layout.yesnoquestion, parent, false);
+
+                RadioGroup radioGroup = (RadioGroup) row.findViewById(R.id.yes_no);
+                radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                        int index = group.indexOfChild(group.findViewById(group.getCheckedRadioButtonId()));
+                        if (index == 0)
+                            Log.d("DEBUG", "Value changed");
+                        viewHolder.rb_checked = checkedId;
+                    }
+                });
+                ((RadioGroup) row.findViewById(R.id.yes_no)).check(viewHolder.rb_checked);
             }
             else {
                 row = inflater.inflate(R.layout.number, parent, false);
+
             }
             viewHolder.line1 = (TextView) row.findViewById(R.id.question);
             row.setTag(viewHolder);
